12828dc75be4f37846348a816ca4c5d608d0377d
Umair Jameel <umairjameel321@gmail.com>
2023-12-11 11:13:49 +0500

    Hello, I have changed the last dots for commit message.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,7 +3,6 @@ module.exports = {
   extends: [],
   rules: {
     'header-min-length': [2, 'always', 20],
-    // Custom rules for capital letter and ending with a comma
     'header-case-start-capital': [2, 'always'],
     'header-end-period': [2, 'always'],
   },
@@ -16,8 +15,8 @@ module.exports = {
             'Commit message must start with a capital letter',
           ];
         },
-        'header-end-period': ({ raw }) => {
-          return [/\.$/.test(raw), 'Commit message must end with a period'];
+        'header-end-period': ({ header }) => {
+          return [/\.$/.test(header), 'Commit message must end with a period'];
         },
       },
     },
